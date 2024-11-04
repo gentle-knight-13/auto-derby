c876127aa9010f976973d08265173899521fe56a
gentle-knight-13 <gentle.knight.13@gmail.com>
2024-11-05 00:24:36 +0900

    chore: Implement sequential file saving for screenshots
    * chore: intellisense

--- scripts/sceenshot.py
+++ scripts/sceenshot.py
@@ -23,17 +23,36 @@ def main():
         dest="dst",
         default="debug/last_screenshot.png",
     )
+    parser.add_argument(
+        "-s",
+        "--separate",
+        dest="separate",
+        action="store_true"
+    )
 
     args = parser.parse_args()
     dst = args.dst
+    is_separate = args.separate
 
     c = clients.DMMClient.find()
     if not c:
         raise RuntimeError("DMM client not running")
     c.setup()
-    clients.set_current(c)
-    template.g.last_screenshot_save_path = dst
-    template.screenshot()
+    clients.set_current(c) # type: ignore
+
+    if is_separate:
+        dirname, basename = os.path.split(dst)
+        filename, ext = os.path.basename(basename).split('.', 1)
+        i = 1
+        while True:
+            filepath = os.path.join(dirname, f"{filename}_{i}.{ext}")
+            if not os.path.exists(filepath):
+                template.g.last_screenshot_save_path = filepath
+                break
+            i += 1
+    else:
+        template.g.last_screenshot_save_path = dst
+    template.screenshot() # type: ignore
 
 
 if __name__ == "__main__":
